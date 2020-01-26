86cc1f01d4c2f9d7148e287ecc7f1a3c8db64c4e
Duane Stanford <stanfordduane@gmail.com>
2020-01-26 16:58:54 -0600

    addded directory creation to deploy.js

--- deploy.js
+++ deploy.js
@@ -6,6 +6,9 @@ const glob = util.promisify(require('glob'));
 const upath = require('upath');
 const fs = require('fs');
 
+const remotePathBase = "/home/ofbagvxe/public_html";
+const ignoredRemoteItems = new Set(['.well-known', 'cgi-bin', '.htaccess', 'favicon.ico']);
+
 
 let itemsToUpload = [];
 
@@ -31,27 +34,28 @@ sftp.connect({
   sftp.end();
   console.error(err);
   process.exit(1);
-  console.log("SFTP Connection Ended");
 });
 
 function scanLocalFiles() {
   let localPublicDir = upath.join(process.cwd(), 'public');
-  console.log(localPublicDir);
+  console.log("Local Public Is: "+ localPublicDir + "\n");
 
   return glob(`${localPublicDir}/**/*`).then(globMatches => {
     let items = globMatches.map(path => {
       return {
         isDirectory: fs.lstatSync(path).isDirectory(),
-        localPath: path
-      }
+        localPath: path,
+        remotePath: upath.join(
+          remotePathBase,
+          upath.relative(localPublicDir, path)
+        )}
     });
-    console.log(items);
     return items;
   })
 }
 
 function cleanRemote() {
-  console.log('\nCleaned Remote Server');
+  console.log('\nNext: Cleaning Remote Server . . .');
 
   return sftp.list(remotePathBase)
     .then(objectList => {
@@ -61,18 +65,38 @@ function cleanRemote() {
       .filter(obj => obj.type === 'd')
       .map(obj => obj.name);
 
-      let operations = directoriesToRemove.map(dir => sftp.rmdir(upath.join(remotePathBase, dir), true)
-    .then(() => console.log(`Removed directory $(dir)`)))
-    .concat(filesToRemove.map(file =>
-      sftp.delete(upath.join(remotePathBase, file))
-    ))
+      let filesToRemove = objectList
+      .filter(obj => obj.type === '-')
+      .map(obj => obj.name);
+
+      let operations = directoriesToRemove.map(dir =>
+        sftp.rmdir(upath.join(remotePathBase, dir), true)
+      .then(() => console.log(`Removed Directory ${dir}`)))
+    .concat(filesToRemove.map(file => sftp.delete(upath.join(remotePathBase, file))
+      .then(() => console.log(`Removed File ${file}`))));
+
+      return Promise.all(operations);
     })
 }
 
 function createDirectoriesFor(items) {
-  console.log("Creating directories");``
+  console.log("\nNext: Creating directories . . . \n");
+
+  let directoriesToCreate = items.filter(path => path.isDirectory);
+
+  console.log(" \'Directories to Create\' is defined as: " + directoriesToCreate);
+
+  return Promise.all(directoriesToCreate.map(dir => sftp.mkdir(dir.remotePath, true)
+.then(() => console.log(`Created Directory ${dir.remotePath}`))));
 }
 
+
 function uploadFiles(items) {
-  console.log("uploading files");
+  console.log("\nUploading files . . . \n");
+
+  let filesToUpload = items.filter(path => !path.isDirectory);
+
+  return Promise.all(filesToUpload.map(file =>
+  sftp.put(file.localPath, file.remotePath)
+  .then(() => console.log(`Uploaded file ${file.remotePath}`))));
 }
